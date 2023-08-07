b8186c25af4a4fd7e22ea2c3be94b829bc1b6554
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-08-07 15:28:21 -0300

    feat: implementing find ProductRepository in store-catalog

--- src/modules/store-catalog/repository/product.repository.spec.ts
+++ src/modules/store-catalog/repository/product.repository.spec.ts
@@ -50,4 +50,21 @@ describe('productRepository test', () => {
     expect(products[1].description).toBe("Product 2 description")
     expect(products[1].salesPrice).toBe(200)
   })
+
+  it('should find a product', async () => {
+    await ProductModel.create({
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      salesPrice: 100,
+    })
+
+    const productRepository = new ProductRepository()
+    const product = await productRepository.find("1")
+
+    expect(product.id.id).toBe("1")
+    expect(product.name).toBe("Product 1")
+    expect(product.description).toBe("Product 1 description")
+    expect(product.salesPrice).toBe(100)
+  })
 })
--- src/modules/store-catalog/repository/product.repository.ts
+++ src/modules/store-catalog/repository/product.repository.ts
@@ -16,7 +16,16 @@ export default class ProductRepository implements ProductGateway {
       })
     )
   }
-  find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.");
+  async find(id: string): Promise<Product> {
+    const product = await ProductModel.findOne({
+      where: { id },
+    })
+
+    return new Product({
+      id: new Id(product.get("id")),
+      name: product.get("name"),
+      description: product.get("description"),
+      salesPrice: product.get("salesPrice"),
+    })
   }
 }
